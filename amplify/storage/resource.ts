d3d80bf1096dbf810c5f77064a87098879ec3377
maxim.tyurin <maxim.tyurin@1win.pro>
2025-02-03 19:34:12 +0500

    f

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,18 +1,17 @@
 import { defineStorage } from "@aws-amplify/backend";
 
 export const storage = defineStorage({
-  name: "storage-browser-test",
+  name: "tgbot-storage-test",
   access: (allow: any) => ({
-    'media-readwritedelete/*': [allow.authenticated.to(['read', 'write', 'delete'])],
-    'media-readonly/*': [allow.authenticated.to(['read'])],
-    'shared-folder-readwrite/*': [
-      allow.authenticated.to(['read', 'write'])
+    'public/*': [
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read',]),
     ],
-    'protected-useronlyreadwritedelete/{entity_id}/*': [
+    'protected/{entity_id}/*': [
       allow.authenticated.to(['read']),
       allow.entity('identity').to(['read', 'write', 'delete'])
     ],
-    'private-useronlyreadwritedelete/{entity_id}/*': [
+    'private/{entity_id}/*': [
       allow.entity('identity').to(['read', 'write', 'delete'])
     ]
   })
